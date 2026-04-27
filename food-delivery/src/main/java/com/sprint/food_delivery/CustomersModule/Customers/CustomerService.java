8a08cd88376f26b44bd42ceb0467c7c7fbf2a1b8
Annie Rufina <anniecharles2004@gmail.com>
2026-04-27 21:55:19 +0530

    updated code

--- food-delivery/src/main/java/com/sprint/food_delivery/CustomersModule/Customers/CustomerService.java
+++ food-delivery/src/main/java/com/sprint/food_delivery/CustomersModule/Customers/CustomerService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sprint.food_delivery.Exception.BadRequestException;
 import com.sprint.food_delivery.Exception.ConflictException;
 import com.sprint.food_delivery.Exception.ResourceNotFoundException;
 
